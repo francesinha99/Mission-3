1c270383324813cf87d69ee00a20aa83a732a111
michaelpiscione <piscionemichael@gmail.com>
2022-01-19 20:46:24 -0700

    mission-3
    mission-3

---
+++ Mission-2/Models/CalculatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission_2.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0, 100)]
+        public int assignments { get; set;}
+        [Range(0, 100)]
+        public int group { get; set; }
+        [Range(0, 100)]
+        public int quizzes { get; set; }
+        [Range(0, 100)]
+        public int exams { get; set; }
+        [Range(0, 100)]
+        public int intex { get; set; }
+        
+    }
+}
---
+++ Mission-2/wwwroot/js/calculator.js
@@ -0,0 +1,52 @@
+﻿$("#btnCalc").click(function () {
+
+    //Get percent grade through calculation
+    let percentGrade = ($("#assignments").val() * .55) + ($("#group").val() * .05) +
+        ($("#quizzes").val() * .10) + ($("#exams").val() * .20) +
+        ($("#intex").val() * .10)
+
+    //Use percent grade to find letter grade
+    let letterGrade = ''
+
+    if (percentGrade >= 94) {
+        letterGrade = 'A'
+    }
+    else if (percentGrade >= 90) {
+        letterGrade = "A-"
+    }
+    else if (percentGrade >= 87) {
+        letterGrade = "B+"
+    }
+    else if (percentGrade >= 84) {
+        letterGrade = "B"
+    }
+    else if (percentGrade >= 80) {
+        letterGrade = "B-"
+    }
+    else if (percentGrade >= 77) {
+        letterGrade = "C+"
+    }
+    else if (percentGrade >= 74) {
+        letterGrade = "C"
+    }
+    else if (percentGrade >= 70) {
+        letterGrade = "C-"
+    }
+    else if (percentGrade >= 67) {
+        letterGrade = "D+"
+    }
+    else if (percentGrade >= 64) {
+        letterGrade = "D"
+    }
+    else if (percentGrade >= 60) {
+        letterGrade = "D-"
+    }
+    else {
+        letterGrade = "E"
+    }
+
+    //Display the letter grade and percent grade to the user
+    alert("Letter Grade: " + letterGrade + "\nPercent Grade: " + percentGrade)
+
+})
+
